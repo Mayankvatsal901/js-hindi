4a2775d5e7bd2c79c78ab8049bd4c2a93a67e861
Mayankvatsal901 <mayankvatsal800@gmail.com>
2025-04-27 20:24:59 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+//number-2 power to 53
+
+ //we are using nodejs,not brwoser
+ //bigint 
+ // string ='',""
+ //boolean=>true/false
+ //null=> standalone value
+ //undefined
+ //symbol for identifying unique
+ 
+ 
+ // object
+ 
+
+ 
+ console.log(typeof "hitesh");
+ console.log(typeof null);
+ //undeefined //undefined
+ //null object
+ 
+
+
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="mayank@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+//accountId=2 //not allow
+   
+
+accountEmail="rohan@gmail.com"
+accountpassword="2121"
+accountcity="up"
+console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountcity])
+
+
+ 
