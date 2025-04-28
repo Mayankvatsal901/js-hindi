0ae0fab08873e1ad20e132d0a0f9cfdeba3e37a3
Mayankvatsal901 <mayankvatsal800@gmail.com>
2025-04-28 12:02:34 +0000

    conversion

---
+++ 01_basics/01_conveersion.js
@@ -0,0 +1,27 @@
+let score=null
+
+// console.log(typeof score);
+// let valueInnumber=Number(score)
+// console.log(valueInnumber)
+// // "33"->33
+// //"33abc"->NaN
+// //true->1,false->0
+// let LoggedIn=1
+// let booleamnLoggedIn=Boolean(LoggedIn)
+// console.log(booleamnLoggedIn)
+// let somenumber=33
+// let stringnum=String(somenumber)
+// console.log(stringnum);
+
+//***************operation*************************
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+//normal operations
+//tricky
+let str1="hello"
+let str2=" hitesh"
+console.log(1+"2");
+console.log("1"+2);
+
+
